0b162dafa39d2e66a8ec539b2436d7b714bdfb93
LukaszProkop <lukasz.marek.prokop@gmail.com>
2020-09-10 19:00:26 +0200

    Refactor decrypting part (homework)

--- src/main/java/com/epam/prejap/oop/ooad/v1/Caesar.java
+++ src/main/java/com/epam/prejap/oop/ooad/v1/Caesar.java
@@ -17,33 +17,56 @@ class Caesar {
             if (input.equals("encrypt")) {
                 input = askForPlainTextMessage(in);
                 output = encryptMessage(input);
-                // output the encrypted message
                 outputEncryptedMessage(output);
                 break;
             } else if (input.equals("decrypt")) {
-                System.out.println("Enter a message to decrypt: ");
-                input = in.nextLine();
-                output = "";
-                for (int i = 0; i < input.length(); i++) {
-                    char letter = input.charAt(i);
-                    if ('A' <= letter && letter <= 'Z') {
-                        if (letter - 3 >= 'A') {
-                            output = output + (char) (letter - 3);
-                        } else {
-                            output = output + (char) (letter - 3 + 26);
-                        }
-                    } else {
-                        output = output + letter;
-                    }
-                }
-                System.out.println("Decrypted message: ");
-                System.out.println(output);
+                input = askForEncryptedMessage(in);
+                output = decryptMessage(input);
+                outputPlainTextMessage(output);
                 break;
             } else {
                 System.out.println("Tertium non datur!");
             }
         } while (true);
         System.out.println("Vale!");
+    }
+
+    private static void outputPlainTextMessage(String output) {
+        System.out.println("Decrypted message: ");
+        System.out.println(output);
+    }
+
+    private static String decryptMessage(String input) {
+        String output;
+        output = "";
+        for (int i = 0; i < input.length(); i++) {
+            char letter = input.charAt(i);
+            char character;
+            character = decryptSingleCharacter(letter);
+            output += character;
+        }
+        return output;
+    }
+
+    private static char decryptSingleCharacter(char letter) {
+        char character;
+        if ('A' <= letter && letter <= 'Z') {
+            if (letter - 3 >= 'A') {
+                character = (char) (letter - 3);
+            } else {
+                character = (char) (letter - 3 + 26);
+            }
+        } else {
+            character = letter;
+        }
+        return character;
+    }
+
+    private static String askForEncryptedMessage(Scanner in) {
+        String input;
+        System.out.println("Enter a message to decrypt: ");
+        input = in.nextLine();
+        return input;
     }
 
     private static void outputEncryptedMessage(String output) {
@@ -56,7 +79,6 @@ class Caesar {
         output = "";
         for (int i = 0; i < input.length(); i++) {
             char letter = input.charAt(i);
-            // encrypts a single character
             char character;
             character = encryptSingleCharacter(letter);
             output += character;
